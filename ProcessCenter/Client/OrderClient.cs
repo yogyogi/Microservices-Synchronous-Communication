692006824349a37df333069e2fbc4f73ed8d0b63
Yogi <yogeshcs2003@gmail.com>
2021-05-10 15:26:00 +0530

    done

---
+++ ProcessCenter/Client/OrderClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static ProcessCenter.Infrastructure.Dtos;
+
+namespace ProcessCenter.Clients
+{
+    public class OrderClient
+    {
+        private readonly HttpClient httpClient;
+        public OrderClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IReadOnlyCollection<OrderDto>> GetOrderAsync()
+        {
+            var items = await httpClient.GetFromJsonAsync<IReadOnlyCollection<OrderDto>>("/order");
+            return items;
+        }
+    }
+}
